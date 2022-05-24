6a113f2840751ef5b0f07a90e9ee53c822bfdef5
LordOfPolls <ddavidallen13@gmail.com>
2022-05-24 18:29:43 +0100

    refactor: Remove wildcard imports from models/discord

--- naff/models/discord/__init__.py
+++ naff/models/discord/__init__.py
@@ -1,26 +1,150 @@
-from .activity import *
-from .application import *
-from .asset import *
-from .channel import *
-from .color import *
-from .components import *
-from .embed import *
-from .emoji import *
-from .enums import *
-from .file import *
-from .guild import *
-from .invite import *
-from .message import *
-from .modal import *
-from .reaction import *
-from .role import *
-from .scheduled_event import *
-from .snowflake import *
-from .stage_instance import *
-from .sticker import *
-from .team import *
-from .thread import *
-from .timestamp import *
-from .user import *
-from .voice_state import *
-from .webhooks import *
+from .activity import ActivityTimestamps, ActivityParty, ActivityAssets, ActivitySecrets, Activity
+from .application import Application
+from .asset import Asset
+from .channel import (
+    ChannelHistory,
+    PermissionOverwrite,
+    MessageableMixin,
+    InvitableMixin,
+    ThreadableMixin,
+    WebhookMixin,
+    BaseChannel,
+    DMChannel,
+    DM,
+    DMGroup,
+    GuildChannel,
+    GuildCategory,
+    GuildNews,
+    GuildText,
+    ThreadChannel,
+    GuildNewsThread,
+    GuildPublicThread,
+    GuildPrivateThread,
+    GuildVoice,
+    GuildStageVoice,
+    process_permission_overwrites,
+    TYPE_ALL_CHANNEL,
+    TYPE_DM_CHANNEL,
+    TYPE_GUILD_CHANNEL,
+    TYPE_THREAD_CHANNEL,
+    TYPE_VOICE_CHANNEL,
+    TYPE_CHANNEL_MAPPING,
+    TYPE_MESSAGEABLE_CHANNEL,
+)
+from .color import (
+    Color,
+    BrandColors,
+    MaterialColors,
+    FlatUIColors,
+    process_color,
+    Colour,
+    BrandColours,
+    MaterialColours,
+    FlatUIColours,
+    process_colour,
+)
+from .components import (
+    BaseComponent,
+    InteractiveComponent,
+    Button,
+    SelectOption,
+    Select,
+    ActionRow,
+    process_components,
+    spread_to_rows,
+    get_components_ids,
+    TYPE_ALL_COMPONENT,
+    TYPE_COMPONENT_MAPPING,
+)
+from .embed import (
+    EmbedField,
+    EmbedAttachment,
+    EmbedAuthor,
+    EmbedFooter,
+    EmbedProvider,
+    Embed,
+    process_embeds,
+)
+from .emoji import PartialEmoji, CustomEmoji, process_emoji_req_format, process_emoji
+from .enums import (
+    WebSocketOPCodes,
+    Intents,
+    UserFlags,
+    ApplicationFlags,
+    TeamMembershipState,
+    PremiumTypes,
+    MessageTypes,
+    MessageActivityTypes,
+    MessageFlags,
+    Permissions,
+    ChannelTypes,
+    ComponentTypes,
+    CommandTypes,
+    InteractionTypes,
+    ButtonStyles,
+    MentionTypes,
+    OverwriteTypes,
+    DefaultNotificationLevels,
+    ExplicitContentFilterLevels,
+    MFALevels,
+    VerificationLevels,
+    NSFWLevels,
+    PremiumTiers,
+    SystemChannelFlags,
+    VideoQualityModes,
+    AutoArchiveDuration,
+    ActivityType,
+    ActivityFlags,
+    Status,
+    StagePrivacyLevel,
+    IntegrationExpireBehaviour,
+    InviteTargetTypes,
+    ScheduledEventPrivacyLevel,
+    ScheduledEventType,
+    ScheduledEventStatus,
+    AuditLogEventType,
+)
+from .file import File, open_file, UPLOADABLE_TYPE
+from .guild import (
+    GuildBan,
+    BaseGuild,
+    GuildWelcome,
+    GuildPreview,
+    Guild,
+    GuildTemplate,
+    GuildWelcomeChannel,
+    GuildIntegration,
+    GuildWidgetSettings,
+    GuildWidget,
+    AuditLogChange,
+    AuditLogEntry,
+    AuditLog,
+    AuditLogHistory,
+)
+from .invite import Invite
+from .message import (
+    Attachment,
+    ChannelMention,
+    MessageActivity,
+    MessageReference,
+    MessageInteraction,
+    AllowedMentions,
+    BaseMessage,
+    Message,
+    process_allowed_mentions,
+    process_message_reference,
+    process_message_payload,
+)
+from .modal import InputText, Modal, ParagraphText, ShortText, TextStyles
+from .reaction import ReactionUsers, Reaction
+from .role import Role
+from .scheduled_event import ScheduledEvent
+from .snowflake import to_snowflake, to_optional_snowflake, to_snowflake_list, SnowflakeObject, Snowflake_Type
+from .stage_instance import StageInstance
+from .sticker import StickerTypes, StickerFormatTypes, StickerItem, Sticker, StickerPack
+from .team import TeamMember, Team
+from .thread import ThreadMember, ThreadList
+from .timestamp import TimestampStyles, Timestamp
+from .user import BaseUser, User, NaffUser, Member
+from .voice_state import VoiceState, VoiceRegion
+from .webhooks import WebhookTypes, Webhook
