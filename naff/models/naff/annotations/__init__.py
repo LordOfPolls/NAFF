9375168daccfe61ac83bf1e7ed794e1d29de8fb4
LordOfPolls <ddavidallen13@gmail.com>
2022-05-24 18:17:59 +0100

    refactor: Remove wildcard imports from annotations

--- naff/models/naff/annotations/__init__.py
+++ naff/models/naff/annotations/__init__.py
@@ -1,2 +1,12 @@
-from .argument import *
-from .slash import *
+from .argument import CMD_ARGS, CMD_AUTHOR, CMD_BODY, CMD_CHANNEL
+from .slash import (
+    slash_attachment_option,
+    slash_bool_option,
+    slash_channel_option,
+    slash_float_option,
+    slash_int_option,
+    slash_mentionable_option,
+    slash_role_option,
+    slash_str_option,
+    slash_user_option,
+)
